dade747c227242e6c13f0752bd75ec02d92aba87
Amjad Khalaf <92156267+Amjad-Khalaf@users.noreply.github.com>
2022-03-07 16:18:08 +0000

    Update speedrun.sh
    Introduced basic multithreading to replace original multi-threading method.

--- speedrun.sh
+++ speedrun.sh
@@ -1,37 +1,17 @@
 #!/usr/bin/env bash
 
-#this is a script to speed up the python inversion-id script
-#the premise is to break the 100k sampled unique kmers from the reference into lists of 5k kmers
-#then run the python script on each of those sublists, finally merging the results together
-#this speeds up the script around 10 times.
+#This is a script that implements multi-threading to the inversion-id script.
+#This should increase running speed significantly.
 
-#split 100k list into 5k chunks
-split -l 5000 sampled_sequence1_unique_kmer sampled_sequence1_unique_kmer_segment
 
-#create different output files for each python run
-touch structural_variation{1..20}.out
+#With this script, $1 is the fasta files with chromosome sequence, $2 is the chromosome name, and $3 is the number of threads/parallel processes to run.
+#memory allocations could be added inside the for loop after "do".
 
-#insert your cluster job submission command
-submit = ""
 
-#run python script on each segment
-$submit "./inversion-id.py sampled_sequence1_unique_kmer_segmentaa $1 structural_variation1.out $2" #1
-$submit "./inversion-id.py sampled_sequence1_unique_kmer_segmentab $1 structural_variation2.out $2" #2
-$submit "./inversion-id.py sampled_sequence1_unique_kmer_segmentac $1 structural_variation3.out $2" #3
-$submit "./inversion-id.py sampled_sequence1_unique_kmer_segmentad $1 structural_variation4.out $2" #4 
-$submit "./inversion-id.py sampled_sequence1_unique_kmer_segmentae $1 structural_variation5.out $2" #5
-$submit "./inversion-id.py sampled_sequence1_unique_kmer_segmentaf $1 structural_variation6.out $2" #6
-$submit "./inversion-id.py sampled_sequence1_unique_kmer_segmentag $1 structural_variation7.out $2" #7
-$submit "./inversion-id.py sampled_sequence1_unique_kmer_segmentah $1 structural_variation8.out $2" #8
-$submit "./inversion-id.py sampled_sequence1_unique_kmer_segmentai $1 structural_variation9.out $2" #9
-$submit "./inversion-id.py sampled_sequence1_unique_kmer_segmentaj $1 structural_variation10.out $2" #10
-$submit "./inversion-id.py sampled_sequence1_unique_kmer_segmentak $1 structural_variation11.out $2" #11
-$submit "./inversion-id.py sampled_sequence1_unique_kmer_segmental $1 structural_variation12.out $2" #12
-$submit "./inversion-id.py sampled_sequence1_unique_kmer_segmentam $1 structural_variation13.out $2" #13
-$submit "./inversion-id.py sampled_sequence1_unique_kmer_segmentan $1 structural_variation14.out $2" #14
-$submit "./inversion-id.py sampled_sequence1_unique_kmer_segmentao $1 structural_variation15.out $2" #15
-$submit "./inversion-id.py sampled_sequence1_unique_kmer_segmentap $1 structural_variation16.out $2" #16
-$submit "./inversion-id.py sampled_sequence1_unique_kmer_segmentaq $1 structural_variation17.out $2" #17
-$submit "./inversion-id.py sampled_sequence1_unique_kmer_segmentar $1 structural_variation18.out $2" #18
-$submit "./inversion-id.py sampled_sequence1_unique_kmer_segmentas $1 structural_variation19.out $2" #19
-$submit "./inversion-id.py sampled_sequence1_unique_kmer_segmentat $1 structural_variation20.out $2" #20
+#split 100k list into chunks with as many jobs as you can submit/threads you have
+let chunk_size=100000/$3
+split -l $chunk_size sampled_sequence1_unique_kmer sampled_sequence1_unique_kmer_segment
+
+#for loop to submit job
+for file in sampled_sequence1_unique_kmer_segment*; do ./inversion-id.py $file $1 $2 &
+done
